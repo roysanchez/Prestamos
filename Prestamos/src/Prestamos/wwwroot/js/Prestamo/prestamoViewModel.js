2c34aadc4e53002994433b95e3d6ecfd02cf473d
Roy Sanchez <roysanchez@outlook.com>
2015-09-14 23:33:34 -0400

    Se implemento parcialmente knockoutjs en la vista de crear prestamos

--- Prestamos/src/Prestamos/wwwroot/js/Prestamo/prestamoViewModel.js
+++ Prestamos/src/Prestamos/wwwroot/js/Prestamo/prestamoViewModel.js
@@ -1,10 +1,34 @@
-﻿var PR2 = PR | {};
+﻿/// <reference path="../../lib/accounting/accounting.js"/>
+/// <reference path="../../lib/knockout/dist/knockout.debug.js"/>
 
-(function (window, ko) {
+var PR = PR || {};
 
-    PR.PrestamoViewModel = function () {
+(function (window, ko, accounting) {
+
+    PR.PrestamoViewModel = function (model, monedas) {
+        model = model || {};
+
         var self = this;
-        self.Monto = ko.observable();
+        self.Moneda = ko.observable(model.Monto != null ? model.Monto.Moneda : null);
+        self.Monto = ko.observable(model.Monto != null ? model.Monto.Monto : null);
+        self.TextoMonto = ko.computed(function () {
+            return accounting.formatMoney(self.Monto(), monedas[self.Moneda()] + "$", 2);
+        });
+
+        self.Tasa = ko.observable(model.Porciento);
+        self.TextoTasa = ko.computed(function () {
+            return accounting.formatMoney(self.Tasa(), '%', 2);
+        });
+
+        self.Mora = ko.observable(model.PorcMora);
+        self.TextoMora = ko.computed(function () {
+            return accounting.formatMoney(self.Mora(), '%', 2);
+        });
+
+        self.Cuotas = ko.observable(model.CantCuotas);
+        self.TextoCuotas = ko.computed(function () {
+            return accounting.formatNumber(self.Cuotas());
+        });
     };
 
-}(this.window, this.ko));
+}(this.window, this.ko, this.accounting));
