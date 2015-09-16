aceee65d3c9be32d64591331a08fc61e88b23986
Roy Sanchez <roysanchez@outlook.com>
2015-09-16 00:43:36 -0400

    Se reintegro el formato dnx 5

--- Prestamos/Prebuild.ps1
+++ Prestamos/Prebuild.ps1
@@ -17,7 +17,7 @@ else
 # install DNX
 # only installs the default (x86, clr) runtime of the framework.
 # If you need additional architectures or runtimes you should add additional calls
-# ex: & $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -r coreclr
+& $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -r coreclr
 & $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -Persistent
 
  # run DNU restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
