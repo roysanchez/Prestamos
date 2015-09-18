b41e30dbbefd079fb3bccbb40603cc32d176013d
Roy Sanchez <roysanchez@outlook.com>
2015-09-17 23:29:00 -0400

    Se migraron las librerias externas de wwwroot/lib a wwwroot/libext para que sean tomadas en git

--- src/Prestamos/wwwroot/_references.js
+++ src/Prestamos/wwwroot/_references.js
@@ -3,3 +3,5 @@
 /// <reference path="js/pickadatefix.js" />
 /// <reference path="js/prestamo/prestamoviewmodel.js" />
 /// <reference path="js/site.js" />
+/// <reference path="libext/jquery-validation-unobtrusive-bootstrap/jquery.validation.unobtrusive.bootstrap.js" />
+/// <reference path="libext/modernizr/modernizr-input.js" />
