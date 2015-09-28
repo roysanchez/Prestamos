c849a49d103288d9b9348462e4d41a76628b19c7
Roy Sanchez <roysanchez@outlook.com>
2015-09-28 19:59:45 -0400

    Se modifico la pantalla de crear prestamos para que busque el nombre de la persona cuando se le ingresa la cedula.

--- src/Prestamos/wwwroot/js/Prestamo/prestamoViewModel.js
+++ src/Prestamos/wwwroot/js/Prestamo/prestamoViewModel.js
@@ -6,14 +6,14 @@
 
 (function (window, $, PR, Vue, accounting) {
 
-    //https://github.com/amsul/pickadate.js/issues/160
-    if (!Modernizr.inputtypes.date) {
-        $('[type="date"]').pickadate({
-            onClose: function () {
-                $('.datepicker').blur();
-            }
-        });
-    }
+    ////https://github.com/amsul/pickadate.js/issues/160
+    //if (!Modernizr.inputtypes.date) {
+    //    $('[type="date"]').pickadate({
+    //        onClose: function () {
+    //            $('.datepicker').blur();
+    //        }
+    //    });
+    //}
 
     var model = PR.PrestamoViewModel;
 
@@ -21,6 +21,7 @@
         el: '#crearPrestamoForm',
         name: 'Creacion-Prestamos',
         data: {
+            deudorId: model.deudorId,
             monto: model.monto,
             textoMonto: "",
             tasa: model.tasa,
@@ -29,7 +30,8 @@
             textoMora: "",
             cuotas: model.cuotas,
             textoCuotas: "",
-            cedula : model.deudorCedula
+            cedula: model.deudorCedula,
+            nombre: model.deudorNombre
         },
         methods: {
             BuscarCliente: BuscarClientePorCedula
@@ -53,13 +55,21 @@
     );
 
     function BuscarClientePorCedula(event) {
-        var cedula = this.cedula;
-        var url = PR.PrestamoViewModel.urlBuscarCliente;
+        var vm = this;
+        var data = event.target.dataset;
 
-        var posting = $.post(url, { cedula: cedula });
-        posting.done(function (cliente) {
-            console.debug("cliente", cliente);
-        });
+        if (vm.cedula !== "") {
+            $.post(data.url, { cedula: vm.cedula })
+                .done(function (cliente) {
+                    console.debug("cliente", cliente);
+                    vm.deudorId = cliente.Id;
+                    vm.nombre = cliente.NombreTitular;
+                });
+        } else {
+            $('#' + data.modal).modal();
+        }
+
+        
     }
 
 }(this.window, this.jQuery, this.PR, this.Vue, this.accounting));
