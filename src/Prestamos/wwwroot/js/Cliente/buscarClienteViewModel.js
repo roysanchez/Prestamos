9932d85a98da6bc3c56c306cf5b15cd6e9f59deb
Roy Sanchez <roysanchez@outlook.com>
2015-09-28 21:36:00 -0400

    Se inicio el trabajo del modal para la busqueda de clientes

--- src/Prestamos/wwwroot/js/Cliente/buscarClienteViewModel.js
+++ src/Prestamos/wwwroot/js/Cliente/buscarClienteViewModel.js
@@ -1,1 +1,30 @@
-﻿
+﻿/// <reference path="../../lib/jquery/dist/jquery.js"/>
+/// <reference path="../../lib/vue/dist/vue.js"/>
+
+(function (window, $, PR, Vue, accounting) {
+
+    var demo = new Vue({
+        el: '#BuscadorClientes',
+        name: 'Buscador-Clientes',
+        data: {
+            cedula: '',
+            nombres: '',
+            apellidos: ''
+
+        },
+        methods: {
+            SeleccionCliente: SeleccionarCliente,
+            BuscarClientes: BuscarClientes
+        }
+    });
+
+    function BuscarClientes(event) {
+        var vm = this;
+    }
+
+    function SeleccionarCliente(event) {
+        var vm = this;
+
+    }
+
+}(this.window, this.jQuery, this.PR, this.Vue, this.accounting));
