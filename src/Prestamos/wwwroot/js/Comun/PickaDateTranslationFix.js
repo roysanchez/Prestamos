5a8dcabb5db59be9492e94bb14be05eca7d17e06
Roy Sanchez <roysanchez@outlook.com>
2015-10-08 19:55:03 -0400

    Se convirtio a modulo AMD

--- src/Prestamos/wwwroot/js/Comun/PickaDateTranslationFix.js
+++ src/Prestamos/wwwroot/js/Comun/PickaDateTranslationFix.js
@@ -1,1 +1,9 @@
-﻿$.fn.pickatime = { defaults: {} };
+﻿/**
+* Modulo que arregla el error de que el objeto defaults no existe, lo saque de un bug no me acuerdo cual
+* @module jquery
+* @module picker
+* @module pickadate
+*/
+define(['jquery', 'picker', 'pickadate'], function () {
+    $.fn.pickatime = { defaults: {} };
+});
