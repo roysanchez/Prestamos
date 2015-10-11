f5764b21870b4bf2d31bf7b3479be22ceacd804e
Roy Sanchez <roysanchez@outlook.com>
2015-10-11 01:15:53 -0400

    Se modifico para que sea un modulo

--- src/Prestamos/wwwroot/js/Comun/PickaDate.js
+++ src/Prestamos/wwwroot/js/Comun/PickaDate.js
@@ -1,16 +1,37 @@
-(function (window, $) {
+/// <reference path="../../lib/jquery/dist/jquery.js"/>
+/// <reference path="../../lib/vue/dist/vue.js"/>
+/// <reference path="../../lib/accounting/accounting.js"/>
+/// <reference path="../../lib/pickadate/lib/picker.js"/>
+/// <reference path="../../lib/pickadate/lib/picker.date.js"/>
 
-    //https://github.com/amsul/pickadate.js/issues/160
+//TODO Mover la parte de pickadate a un modulo independiente comun
+
+/**
+* Modulo para configurar el datepicker pickadate
+* @module jquery
+* @module Modernizr
+* @module require
+*/
+define(['jquery', 'Modernizr', 'require'], function ($, Modernizr, require) {
+
     if (!Modernizr.inputtypes.date) {
-        $('[type="date"]').pickadate({
-            format: 'dd-mm-yyyy',
-            selectYears: 15,
-            selectMonths: true,
-            min: new Date(1960, 0, 1),
-            onClose: function () {
-                $('.datepicker').blur();
-            }
+        /**
+        * Modulo para cargar los datepicker pickadate
+        * @module picker
+        * @module pickadate
+        * @module pickerES
+        */
+        require(['picker', 'pickadate', 'pickerES'], function () {
+            //https://github.com/amsul/pickadate.js/issues/160
+            $('[type="date"]').pickadate({
+                format: 'dd-mm-yyyy',
+                selectYears: 15,
+                selectMonths: true,
+                min: new Date(1960, 0, 1),
+                onClose: function () {
+                    $('.datepicker').blur();
+                }
+            });
         });
     }
-
-}(this.window, this.jQuery));
+});
