1ecc6a733f45cf8c82ad187da2db593c179d9403
Roy Sanchez <roysanchez@outlook.com>
2015-10-11 01:16:06 -0400

    Se movio la configuracion de la creacion local a la creacion global

--- src/Prestamos/wwwroot/js/site.js
+++ src/Prestamos/wwwroot/js/site.js
@@ -3,7 +3,11 @@ var require = {
     paths: {
         lib: '../lib',
         libext: '../libext',
-        jquery: '../lib/jquery/dist/jquery'
+        jquery: '../lib/jquery/dist/jquery',
+        picker: '../lib/pickadate/lib/picker',
+        pickadate: '../lib/pickadate/lib/picker.date',
+        'jquery.validate': '../lib/jquery-validation/dist/jquery.validate',
+        'jquery.validate.unobtrusive': '../lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive',
     },
     enforceDefine: true
 };
