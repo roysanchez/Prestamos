6c01d9a5030df7f9945d628cac1d264e00a8ee55
Roy Sanchez <roysanchez@outlook.com>
2016-02-28 23:35:45 -0400

    Se agrego la configuracion del plugin aurelia validate

--- src/Prestamos.Web/wwwroot/app.js
+++ src/Prestamos.Web/wwwroot/app.js
@@ -3,9 +3,8 @@
         config.title = 'Prestamos';
         config.map([
             {route: ['', 'home'], name: 'home', moduleId: 'app/home', nav: true, title: 'Home'},
-            {route: 'cliente', name: 'cliente', moduleId: 'app/cliente/listar', nav: true, title: 'Cliente' },
-            {route: 'cliente/crear', name: 'crear', moduleId: 'app/cliente/crear', nav: true, title: 'Cliente Crear' }
-            //{route: ['', 'cliente'], name: 'app/cliente/list', moduleId: 'app/cliente/list', nav: true, title: 'Cliente'}
+            {route: 'cliente', name: 'cliente', moduleId: 'app/cliente/index', nav: true, title: 'Clientes' },
+            {route: 'cliente/crear', name: 'cliente-crear', moduleId: 'app/cliente/crear', title: 'Crear nuevo cliente' }
         ]);
 
         this.router = router;
---
+++ src/Prestamos.Web/wwwroot/aurelia-config.js
@@ -0,0 +1,12 @@
+﻿//import {ValidateCustomAttributeViewStrategy} from 'aurelia-validation';
+// https://github.com/aurelia/validation/issues/165
+import {TWBootstrapViewStrategy} from 'aurelia-validation/strategies/twbootstrap-view-strategy';
+
+export function configure(aurelia) {
+    aurelia.use
+      .standardConfiguration()
+      .developmentLogging()
+      .plugin('aurelia-validation', (config) => { config.useViewStrategy(TWBootstrapViewStrategy.AppendToInput); });
+
+    aurelia.start().then(() => aurelia.setRoot());
+}
