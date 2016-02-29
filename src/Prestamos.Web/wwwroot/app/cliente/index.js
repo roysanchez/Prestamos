6eb51a71f5b03cc460456518c5bc20452ccedc9c
Roy Sanchez <roysanchez@outlook.com>
2016-02-28 23:38:13 -0400

    Se les cambio el nombre para que sea mas facil entender que es el punto de entrada de los clientes.
    Se cambio para que utilice la factoria de clientes y se corrigio el link de creacio en la pantalla.

--- src/Prestamos.Web/wwwroot/app/cliente/index.js
+++ src/Prestamos.Web/wwwroot/app/cliente/index.js
@@ -1,16 +1,17 @@
 ﻿import {inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-fetch-client'
-import {Cliente} from './cliente' 
+import {ClienteFactory} from './cliente' 
 
-@inject(HttpClient)
+@inject(HttpClient, ClienteFactory)
 class List {
-    constructor(http){
+    constructor(http, factory){
         this.http = http;
+        this.factory = factory;
         this.clientes = [];
     }
 
     activate(){
-        var addCliente = cl => this.clientes = cl.map(c => new Cliente(c));
+        var addCliente = cl => this.clientes = cl.map(c => this.factory.Make(c));
         return this.http.fetch('http://localhost:5001/api/Cliente', { mode: 'cors' })
             .then(resp => resp.json().then(addCliente));
     }
