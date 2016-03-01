e25399cba22b98c6bed2b03fe15e07a4cccadceb
Roy Sanchez <roysanchez@outlook.com>
2016-03-01 00:02:01 -0400

    Se creo la vista de editar cliente

--- src/Prestamos.Web/wwwroot/app.js
+++ src/Prestamos.Web/wwwroot/app.js
@@ -4,7 +4,9 @@
         config.map([
             {route: ['', 'home'], name: 'home', moduleId: 'app/home', nav: true, title: 'Home'},
             {route: 'cliente', name: 'cliente', moduleId: 'app/cliente/index', nav: true, title: 'Clientes' },
-            {route: 'cliente/crear', name: 'cliente-crear', moduleId: 'app/cliente/crear', title: 'Crear nuevo cliente' }
+            {route: 'cliente/crear', name: 'cliente-crear', moduleId: 'app/cliente/crear', title: 'Crear nuevo cliente' },
+            {route: 'cliente/editar/:id', name: 'cliente-editar', moduleId: 'app/cliente/editar', title: 'Editar cliente' }
+
         ]);
 
         this.router = router;
---
+++ src/Prestamos.Web/wwwroot/app/cliente/editar.js
@@ -0,0 +1,15 @@
+﻿import {inject} from 'aurelia-framework';
+import {ClienteFactory} from './cliente';
+
+@inject(ClienteFactory)
+class Editar {
+    constructor(factory){
+        this.factory = factory;
+    }
+    //TODO; Cargar los datos del servicio de consulta
+    activate(params) {
+        this.id = params.id;
+    }
+}
+
+export {Editar};
