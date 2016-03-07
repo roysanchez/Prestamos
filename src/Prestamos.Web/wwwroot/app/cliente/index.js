b6d720927969eeb1fcacc825fcfac6ad78662150
Roy Sanchez <roysanchez@outlook.com>
2016-03-06 21:52:07 -0400

    Se agrego el llamado a datatables

--- src/Prestamos.Web/wwwroot/app/cliente/index.js
+++ src/Prestamos.Web/wwwroot/app/cliente/index.js
@@ -1,19 +1,27 @@
 ﻿import {inject} from 'aurelia-framework';
-import {HttpClient} from 'aurelia-fetch-client'
-import {ClienteFactory} from './cliente' 
+import {HttpClient} from 'aurelia-fetch-client';
+import {jQuery} from 'jquery';
+import dataTable from 'datatables.net-bs4';
+import {ClienteFactory} from './cliente';
 
-@inject(HttpClient, ClienteFactory)
+@inject(HttpClient, ClienteFactory, jQuery)
 class List {
-    constructor(http, factory){
+    constructor(http, factory, $){
         this.http = http;
         this.factory = factory;
+        this.$ = $;
+        this.dt = dataTable(window, $);
         this.clientes = [];
     }
 
-    activate(){
+    activate() {
         var addCliente = cl => this.clientes = cl.map(c => this.factory.Make(c));
         return this.http.fetch('http://localhost:5001/api/Cliente', { mode: 'cors' })
             .then(resp => resp.json().then(addCliente));
+    }
+
+    attached() {
+        this.$('.table').dataTable();
     }
 }
 
